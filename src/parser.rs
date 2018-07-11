ee3b8fb1a1cd36153404a4e5fb4413d98ad27f7c
Jeremy Day <jadaytime@gmail.com>
2018-07-11 17:58:48 -0400

    rewrite parser from scratch
    the initial methodology for parsing was flawed. this is a complete
    rewrite from the bottom up (i.e. starting with terms then moving onto
    operators)
    
    as of this commit, constants and variables are supported, as well as
    parenthetical expressions (containing only constants and variables) and
    concatenation as a multiplication operator
    
    e.g. "3(3x)" will parse successfully

--- src/parser.rs
+++ src/parser.rs
@@ -2,108 +2,111 @@ use expression::*;
 use nom;
 use nom::types::CompleteStr;
 
-pub fn parse_double(input: CompleteStr) -> nom::IResult<CompleteStr, f64> {
-    flat_map!(input, call!(nom::recognize_float), parse_to!(f64))
-}
+named!(parse_double<CompleteStr, f64>,
+    flat_map!(call!(nom::recognize_float), parse_to!(f64))
+);
 
-pub fn parse_constant(input: CompleteStr) -> nom::IResult<CompleteStr, ExpressionNode> {
+named!(parse_constant<CompleteStr, ExpressionNode>,
     do_parse!(
-        input,
-        value: parse_double >> (ExpressionNode::ConstantExprNode { value: value })
+        value: parse_double >>
+        (ExpressionNode::ConstantExprNode { value, })
     )
-}
+);
 
-pub fn parse_exponentiation(input: CompleteStr) -> nom::IResult<CompleteStr, ExpressionNode> {
+named!(parse_variable<CompleteStr, ExpressionNode>,
     do_parse!(
-        input,
-        left_node: take_until!("^") >> tag!("^") >> right_node: take_while!(|x| true)
-            >> (ExpressionNode::BinaryExprNode {
-                operator: BinaryOperator::Exponentiation,
-                left_node: Box::new(parse_expr(left_node).unwrap().1),
-                right_node: Box::new(parse_expr(right_node).unwrap().1),
-            })
+        var: take_while1!(|x| nom::is_alphabetic(x as u8)) >>
+        (ExpressionNode::VariableExprNode { variable_key: var.to_string(), })
     )
-}
+);
 
-pub fn parse_multiplication(input: CompleteStr) -> nom::IResult<CompleteStr, ExpressionNode> {
+named!(parse_constant_coefficient<CompleteStr, ExpressionNode>,
     do_parse!(
-        input,
-        left_node: take_until!("*") >> tag!("*") >> right_node: take_while!(|x| true)
-            >> (ExpressionNode::BinaryExprNode {
-                operator: BinaryOperator::Multiplication,
-                left_node: Box::new(parse_expr(left_node).unwrap().1),
-                right_node: Box::new(parse_expr(right_node).unwrap().1),
-            })
+        coefficient: parse_constant >>
+        term: parse_term >>
+        (ExpressionNode::BinaryExprNode {
+            operator: BinaryOperator::Multiplication,
+            left_node: Box::new(coefficient),
+            right_node: Box::new(term),
+        })
     )
-}
+);
 
-pub fn parse_division(input: CompleteStr) -> nom::IResult<CompleteStr, ExpressionNode> {
+named!(parse_variable_coefficient<CompleteStr, ExpressionNode>,
     do_parse!(
-        input,
-        left_node: take_until!("/") >> tag!("/") >> right_node: take_while!(|x| true)
-            >> (ExpressionNode::BinaryExprNode {
-                operator: BinaryOperator::Division,
-                left_node: Box::new(parse_expr(left_node).unwrap().1),
-                right_node: Box::new(parse_expr(right_node).unwrap().1),
-            })
+        coefficient: parse_variable >>
+        term: parse_term >>
+        (ExpressionNode::BinaryExprNode {
+            operator: BinaryOperator::Multiplication,
+            left_node: Box::new(coefficient),
+            right_node: Box::new(term),
+        })
     )
-}
+);
 
-pub fn parse_addition(input: CompleteStr) -> nom::IResult<CompleteStr, ExpressionNode> {
-    do_parse!(
-        input,
-        left_node: take_until!("+") >> tag!("+") >> right_node: take_while!(|x| true)
-            >> (ExpressionNode::BinaryExprNode {
-                operator: BinaryOperator::Addition,
-                left_node: Box::new(parse_expr(left_node).unwrap().1),
-                right_node: Box::new(parse_expr(right_node).unwrap().1),
-            })
-    )
-}
-
-pub fn parse_subtraction(input: CompleteStr) -> nom::IResult<CompleteStr, ExpressionNode> {
-    do_parse!(
-        input,
-        left_node: take_until!("-") >> tag!("-") >> right_node: take_while!(|x| true)
-            >> (ExpressionNode::BinaryExprNode {
-                operator: BinaryOperator::Subtraction,
-                left_node: Box::new(parse_expr(left_node).unwrap().1),
-                right_node: Box::new(parse_expr(right_node).unwrap().1),
-            })
-    )
-}
-
-pub fn parse_binary(input: CompleteStr) -> nom::IResult<CompleteStr, ExpressionNode> {
+named!(parse_coefficient<CompleteStr, ExpressionNode>,
     alt_complete!(
-        input,
-        parse_addition | parse_subtraction | parse_multiplication | parse_division
-            | parse_exponentiation
+        parse_constant_coefficient |
+        parse_variable_coefficient
     )
-}
+);
 
-fn parse_expr(input: CompleteStr) -> nom::IResult<CompleteStr, ExpressionNode> {
-    alt_complete!(input, parse_binary | parse_constant)
+named!(parse_parens<CompleteStr, ExpressionNode>,
+    delimited!( char!('('), parse_expr, char!(')') )
+);
+
+named!(parse_expr<CompleteStr, ExpressionNode>,
+    call!(parse_term)
+);
+
+named!(parse_term<CompleteStr, ExpressionNode>,
+    alt_complete!(
+        parse_coefficient |
+        parse_parens      |
+        parse_variable    |
+        parse_constant
+    )
+);
+
+use std::collections::HashMap;
+#[test]
+fn test_parse_constant() {
+    assert_eq!(
+        parse_constant(CompleteStr("3")).unwrap().1.evaluate(&HashMap::new()),
+        3.0
+    );
+
+    let mut vars_map = HashMap::new();
+    vars_map.insert("x".to_string(), 10.0);
+
+    assert_eq!(
+        parse_variable(CompleteStr("x")).unwrap().1.evaluate(&vars_map),
+        10.0
+    );
 }
 
 #[test]
-fn test_parse_expr() {
+fn test_parse_term() {
+    let mut vars_map = HashMap::new();
+    vars_map.insert("x".to_string(), 10.0);
+
     assert_eq!(
-        parse_expr(CompleteStr("2*7^2")).unwrap().1.evaluate(&[]),
-        98.0
+        parse_expr(CompleteStr("(3(3))")).unwrap().1.evaluate(&HashMap::new()),
+        9.0
     );
 
     assert_eq!(
-        parse_expr(CompleteStr("49-7^2")).unwrap().1.evaluate(&[]),
-        0.0
+        parse_expr(CompleteStr("3x")).unwrap().1.evaluate(&vars_map),
+        30.0
     );
 
     assert_eq!(
-        parse_expr(CompleteStr("10-2+4")).unwrap().1.evaluate(&[]),
-        12.0
+        parse_expr(CompleteStr("3(3(3))")).unwrap().1.evaluate(&HashMap::new()),
+        27.0
     );
 
     assert_eq!(
-        parse_expr(CompleteStr("50/2*10")).unwrap().1.evaluate(&[]),
-        250.0
+        parse_expr(CompleteStr("3(x(3))")).unwrap().1.evaluate(&vars_map),
+        90.0
     );
 }
